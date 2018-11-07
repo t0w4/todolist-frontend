26c5535f219ec1733d2d562b482768675255e775
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-11-08 03:27:33 +0900

    mutation仮登録

---
+++ src/store/mutation-types.ts
@@ -0,0 +1,4 @@
+export const FETCH_ALL_TASKS:string = 'FETCH_ALL_TASKS'
+export const CREATE_TASK: string = 'CREATE_TASK'
+export const UPDATE_TASK:string = 'UPDATE_TASK'
+export const DELETE_TASK:string = 'DELETE_TASK'
---
+++ src/store/mutations.ts
@@ -0,0 +1,21 @@
+import * as types from './mutation-types';
+
+// 短縮メソッド定義:      https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Functions_and_function_scope/Method_definitions
+// 計算されたプロパティ名: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Object_initializer
+export default {
+  [types.FETCH_ALL_TASKS] (state, payload) {
+    throw new Error('FETCH_ALL_TASKS mutation is not implemented')
+  },
+
+  [types.CREATE_TASK] (state, payload) {
+    throw new Error('CREATE_TASK mutation is not implemented')
+  },
+
+  [types.UPDATE_TASK] (state, payload) {
+    throw new Error('UPDATE_TASK mutation is not implemented')
+  },
+
+  [types.DELETE_TASK] (state, payload) {
+    throw new Error('DELETE_TASK mutation is not implemented')
+  }
+}
