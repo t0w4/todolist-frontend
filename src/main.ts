b9e78f362598bdbb78a9c2d496cb1c45b148407f
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-11-08 00:39:52 +0900

    Vuex用の雛形作成

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
+import 'es6-promise'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
 new Vue({
   router,
