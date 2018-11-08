1be964b3d7d705cbc37e9490d0d43b8ae28183b2
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-11-09 03:04:01 +0900

    action仮登録

---
+++ src/store/actions.ts
@@ -0,0 +1,20 @@
+import * as types from './mutation-types';
+import Task from '../api';
+
+export default {
+  fetchTasks: ({ commit }) => {
+    throw new Error('fetchTasks action is not implemented');
+  },
+
+  createTask: ({ commit }) => {
+    throw new Error('createTask action is not implemented');
+  },
+
+  updateTask: ({ commit }) => {
+    throw new Error('updateTask action is not implemented');
+  },
+
+  deleteTask: ({ commit }) => {
+    throw new Error('deleteTask action is not implemented');
+  },
+}
