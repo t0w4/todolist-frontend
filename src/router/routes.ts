84e1c5fa676811ce3e833f594e48b14b9404246f
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-11-11 00:39:51 +0900

    不要なvue-routerのimportを削除

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,6 @@
 import Vue from 'vue';
-import Router from 'vue-router';
 import TodoList from '@/components/templates/TodoList.vue';
 import TodoListDetail from '@/components/templates/TodoListDetail.vue';
-
-Vue.use(Router);
 
 export default [
   {
