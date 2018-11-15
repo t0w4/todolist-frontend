56a3a9a0b01def18b67cf10952863549db712197
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-11-16 02:54:46 +0900

    status別のtasksをTaskColumnとして切り出し

--- src/components/templates/TodoList.vue
+++ src/components/templates/TodoList.vue
@@ -2,60 +2,21 @@
     <div class="tasks-view">
       <h1>todo lists</h1>
       <div class="tasks-list-wrapper">
-        <div class="tasks-column">
-          <h2 class="task-status">
-            <span class="task-text">todo</span>
-          </h2>
-          <ul class="task-list">
-            <li v-for="task in tasks_todo" :key="task.id">
-              <TaskCard v-bind:task="task"/>
-            </li>
-          </ul>
-        </div>
-
-        <div class="tasks-column">
-          <h2 class="task-status">
-            <span class="task-text">in progress</span>
-          </h2>
-          <ul class="task-list">
-            <li v-for="task in tasks_in_progress" :key="task.id">
-              <TaskCard v-bind:task="task"/>
-            </li>
-          </ul>
-        </div>
-
-        <div class="tasks-column">
-          <h2 class="task-status">
-            <span class="task-text">in review</span>
-          </h2>
-          <ul class="task-list">
-            <li v-for="task in tasks_in_review" :key="task.id">
-              <TaskCard v-bind:task="task"/>
-            </li>
-          </ul>
-        </div>
-
-        <div class="tasks-column">
-          <h2 class="task-status">
-            <span class="task-text">done</span>
-          </h2>
-          <ul class="task-list">
-            <li v-for="task in tasks_done" :key="task.id">
-              <TaskCard v-bind:task="task"/>
-            </li>
-          </ul>
-        </div>
+        <TaskColumn v-bind:tasks="tasks_todo">todo</TaskColumn>
+        <TaskColumn v-bind:tasks="tasks_in_progress">in progress</TaskColumn>
+        <TaskColumn v-bind:tasks="tasks_in_review">in review</TaskColumn>
+        <TaskColumn v-bind:tasks="tasks_done">done</TaskColumn>
       </div>
     </div>
 </template>
 
 <script lang='ts'>
 import { mapState } from 'vuex';
-import TaskCard from '@/components/molecules/TaskCard.vue';
+import TaskColumn from '@/components/organsms/TaskColumn.vue';
 export default {
   name: 'TodoList',
   components: {
-    TaskCard,
+    TaskColumn,
   },
 
   computed: {
@@ -91,29 +52,5 @@ export default {
 <style scoped lang="stylus">
 .tasks-list-wrapper {
   display: flex;
-}
-
-.tasks-column {
-  height: 100vh;
-  width: 100%;
-  overflow: hidden;
-  padding: 0 0.5em; /* 外枠と文字との距離 */
-  margin: 0.5em 0.3em; /* 外枠からのmargin */
-  background: #FFF;
-  border: solid 3px rgb(143, 135, 135);/*線*/
-  border-radius: 20px;/*角の丸み*/
-}
-
-.task-status {
-  text-align: left;
-}
-
-.task-text {
-  border-bottom: double 6px green;
-}
-
-.task-list {
-  list-style: none;
-  padding-left: 2px;
 }
 </style>
